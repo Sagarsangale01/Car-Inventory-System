eca747998ca9a2d61eced9f6974a3824098cecfd
Sanglesagar <sagarsangale111@gmail.com>
2018-11-07 18:43:59 +0530

    Add files via upload

---
+++ ckeditor/config.js
@@ -0,0 +1,19 @@
+/**
+ * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	config.extraPlugins = 'tab';
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	config.tabSpaces = 2; // or some other value
+
+	config.wsc_cmd  = 'thes';
+	//config.tabSpaces = 4; // or some other value
+	//config.TabShow = true;
+	
+	config.height = 100;        // 500 pixels high.
+    //config.height = '25em';     // CSS unit (em).
+};
